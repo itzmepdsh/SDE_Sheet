add520378cae677c087fe8b8acf4558b6fc5b58a
itzmepdsh <pdtrigologarithmic7065@gmail.com>
2025-06-15 01:02:57 +0530

    Recursion-1 is completed

--- backtracking.cpp
+++ backtracking.cpp
@@ -27,3 +27,129 @@ public:
     }
 };
 
+//Rat in a Maze
+class Solution {
+  public:
+    void solve(vector<vector<int>>& maze, int m ,int n, int i ,int j,string& path, vector<string>& res){
+        //base case
+        if(i==m-1 && j==n-1 && maze[i][j] != 0){
+            res.push_back(path);
+            return;
+        }
+        
+        //recursive case
+        if(i==-1 || j==-1 || i==m || j==n || maze[i][j] != 1){
+            return;
+        }
+        maze[i][j] = 2;//visited marked
+        int dx[4] = {0,0,-1,1};
+        int dy[4] = {1,-1,0,0};
+        string dir = "RLUD";
+        for(int k=0;k<4;k++){
+            path += dir[k];
+            int x = i+dx[k],y =j+dy[k];
+            solve(maze,m,n,x,y,path,res);
+            path.pop_back();
+        }
+        maze[i][j] = 1;//unmarked
+    }
+    vector<string> ratInMaze(vector<vector<int>>& maze) {
+        // code here
+        vector<string> res;
+        string path = "";
+        int m = maze.size(), n = maze[0].size();
+        solve(maze,m,n,0,0,path,res);
+        sort(res.begin(),res.end());
+        return res;
+    }
+};
+
+//N-Queens
+class Solution {
+public:
+    void solve(vector<int>& rows, vector<vector<string>>& res,
+               vector<string>& board, int n, int r) {
+        // base case
+        if (r == n) {
+            res.push_back(board);
+            return;
+        }
+        // recursive case
+        for (int j = 0; j < n; j++) {
+            int i = 0;
+            for (; i < r; i++) {
+                if (rows[i] == j || i + rows[i] == r + j ||
+                    i - rows[i] == r - j) {
+                    break;
+                }
+            }
+            if (i == r) {
+                rows[r] = j;
+                board[r][j] = 'Q';
+                solve(rows,res,board,n,r+1);
+            }
+            rows[r] = -1;
+            board[r][j] = '.';
+        }
+    }
+
+    vector<vector<string>> solveNQueens(int n) {
+        vector<int> rows(n, -1);
+        vector<string> board;
+        for (int i = 0; i < n; i++) {
+            string row = "";
+            for (int j = 0; j < n; j++) {
+                row += '.';
+            }
+            board.push_back(row);
+        }
+        vector<vector<string>> res;
+        solve(rows, res, board, n, 0);
+        return res;
+    }
+};
+
+//Sudoku Solver
+class Solution {
+public:
+    bool isValid(vector<vector<char>>& board, int i, int j, char num){
+        for(int r=0;r<9;r++){
+            if(board[r][j] == num)return false;
+        }
+        for(int c=0;c<9;c++){
+            if(board[i][c] == num)return false;
+        }
+        int si = (i/3)*3, sj=(j/3)*3;
+        for(int x=si;x<si+3;x++){
+            for(int y=sj;y<sj+3;y++){
+                if(board[x][y] == num)
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    bool solve(vector<vector<char>>& board, int i, int j){
+        //base case
+        if(i==9)
+            return true;
+        //recursive case
+        if(j==9)
+            return solve(board,i+1,0);
+        if(board[i][j] != '.')
+            return solve(board,i,j+1);
+        for(int num = 1;num <=9 ;num++){
+            if(isValid(board,i,j,num+'0')){
+                board[i][j] = num+'0';
+                if(solve(board,i,j+1))
+                    return true;
+                board[i][j] = '.';
+            }
+        }
+        return false;
+    }
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board,0,0);
+    }
+};
+
--- recursion_1.cpp
+++ recursion_1.cpp
@@ -30,3 +30,152 @@ public:
         return res;
     }
 };
+
+//subsets
+class Solution {
+public:
+    void solve(vector<int>& nums, vector<int>& subset, vector<vector<int>>& res, int n, int i){
+        //base case
+        if(i==n){
+            res.push_back(subset);
+            return;
+        }
+        //recursive case
+        solve(nums,subset,res,n,i+1);
+        subset.push_back(nums[i]);
+        solve(nums,subset,res,n,i+1);
+        subset.pop_back();
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> subset;
+        vector<vector<int>> res;
+        int n = nums.size();
+        solve(nums,subset,res,n,0);
+        return res;
+    }
+};
+
+//subsets-2
+class Solution {
+public:
+    void solve(vector<int>& nums, int n, vector<int> subset, vector<vector<int>>& res, int i){
+        res.push_back(subset);
+        
+        //recursive case
+        for(int j=i;j<n;j++){
+            if(j>i && nums[j] == nums[j-1])
+                continue;
+            subset.push_back(nums[j]);
+            solve(nums,n,subset,res,j+1);
+            subset.pop_back(); //backtrack
+        }
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+        vector<int> subset;
+        vector<vector<int>> res;
+        solve(nums,n,subset,res,0);
+        return res;
+    }
+};
+
+//Combination Sum
+class Solution {
+public:
+    void solve(vector<int>& nums, int n, int target, vector<int>& comb, vector<vector<int>>& res, int crsum, int idx){
+        //base case
+        if(crsum == target){
+            res.push_back(comb);
+            return;
+        }
+        if(crsum > target)
+            return;
+        //recursive case
+        for(int j=idx;j<n;j++){
+            comb.push_back(nums[j]);
+            solve(nums, n, target, comb,res,crsum+nums[j],j);
+            comb.pop_back();
+        }
+    }
+    
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<int> comb;
+        vector<vector<int>> res;
+        int crsum = 0;
+        solve(nums,n,target,comb,res,crsum,0);
+        return res;
+    }
+};
+
+//Combination Sum-2 
+class Solution {
+public:
+    void solve(vector<int> nums, int n, int target, int crsum, vector<int> comb, vector<vector<int>>& res, int idx){
+        //base case
+        if(crsum == target){
+            res.push_back(comb);
+            return;
+        }
+        if(crsum > target)
+            return; 
+        //recursive case
+        for(int j=idx;j<n;j++){
+            if(j>idx && nums[j] == nums[j-1])
+                continue;
+            comb.push_back(nums[j]);
+            solve(nums,n,target,crsum+nums[j],comb,res,j+1);
+            comb.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum2(vector<int>& nums, int target) {
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+        int crsum = 0;
+        vector<int> comb;
+        vector<vector<int>> res;
+        solve(nums,n,target,crsum,comb,res,0);
+        return res;
+    }
+};
+
+//Palindrome Partitioning
+class Solution {
+public:
+    bool isPalindrome(string s){
+        int i=0,j=s.length()-1;
+        while(i<j){
+            if(s[i]!=s[j])
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    void solve(string s, int n, vector<string> part, vector<vector<string>>& res, int idx){
+        //base case
+        if(idx == n){
+            res.push_back(part);
+            return;
+        }
+        //recursive case
+        for(int len=1;len<=n-idx;len++){
+            string subs = s.substr(idx,len);
+            if(isPalindrome(subs)){
+                part.push_back(subs);
+                solve(s,n,part,res,idx+len);
+                part.pop_back();
+            }
+        }
+    }
+    vector<vector<string>> partition(string s) {
+        int n = s.length();
+        vector<string> part;
+        vector<vector<string>> res;
+        solve(s,n,part,res,0);
+        return res;
+    }
+};
+
